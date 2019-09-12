f20162620daa8b25b308a8cf6267a869230171f6
Brett Bonnet <brettbonnet4@gmail.com>
2019-09-12 07:57:48 -0400

    Update main.rb

--- main.rb
+++ main.rb
@@ -1,6 +1,17 @@
     
 require './lib/game_board.rb'
 require './lib/knight.rb'
+require './lib/search_tree.rb'
+require './lib/node.rb'
 
-play = GameBoard.new
-play.display_board
+
+
+new_board = GameBoard.new
+new_board.knight_moves([0, 0], [4, 4])
+
+
+
+
+
+
+
